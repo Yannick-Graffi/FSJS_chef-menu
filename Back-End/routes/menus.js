d6fdbed953e3dd982f4c3812b12b822c980492fe
Yannick Graffi <yann001@hotmail.com>
2022-07-24 19:26:06 +0200

    add auth middleware in routes

--- Back-End/routes/menus.js
+++ Back-End/routes/menus.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const menuController = require('../controllers/menuController')
+const Auth = require('../middlwares/auth')
 
-router.get('/', menuController.getMenu)
-router.post('/', menuController.postMenu)
+router.get('/', Auth.isUser, menuController.getMenu)
+router.post('/', Auth.isUser, menuController.postMenu)
 
 module.exports = router;
--- Back-End/routes/tables.js
+++ Back-End/routes/tables.js
@@ -3,9 +3,9 @@ var router = express.Router();
 const tableController = require('../controllers/tableController')
 const Auth = require('../middlwares/auth')
 
-router.get('/', tableController.getTables);
+router.get('/', Auth.isUser, tableController.getTables);
 router.post('/', Auth.isUser, tableController.postTable);
-router.put('/:id', tableController.updateTable);
-router.delete('/:id', tableController.deleteTable);
+router.put('/:id', Auth.isUser, tableController.updateTable);
+router.delete('/:id', Auth.isUser, tableController.deleteTable);
 
 module.exports = router;
