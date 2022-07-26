84251f67c1db120f81d93ea40cc7ba2f9e3dfa02
unknown <delestradej@gmail.com>
2022-07-26 12:02:55 +0200

    fix

--- Front_End/src/components/QRCode.js
+++ Front_End/src/components/QRCode.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import QRCode from "react-qr-code";
-<<<<<<< HEAD
-=======
-import App from '../App'
-
->>>>>>> d479806cc79f536ecb1be025880485c12dca2e68
 import "./QRCode.css";
 
 function qrCode() {
